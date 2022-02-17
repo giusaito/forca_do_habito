13b962670d27603698cc0dfa4d7459d275297424
Giuliano Saito <giulianosaito@gmail.com>
2022-02-17 11:59:01 -0300

    Adicionado o controller Categorias e instalado o pacote react

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['state' => 0, 'message' => 'Unauthenticated.'], 401);
+        }
+        return redirect()->guest(route('auth.login'));
     }
 }
---
+++ app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+use App\Http\Resources\Categoria as CategoriaResource;
+use App\Http\Resources\CategoriaCollection;
+
+class CategoriasController extends Controller
+{
+    public function index()
+    {
+        return new CategoriaCollection(Categoria::all());
+    }
+
+    public function show(Categoria $categoria)
+    {
+        return response()->json([
+            'categoria'=>$categoria
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|max:255',
+        ],[
+            'nome.required'         => 'Por favor, escreva o nome da categoria',
+            'nome.max'              => 'O nome da categoria pode ter no máximo 255 caracteres'
+        ]);
+
+        $categoria = Categoria::create($request->all());
+
+        return response()->json([
+            'message'=>'Categoria cadastrada com sucesso!'
+        ]);
+    }
+    public function update(Request $request, Categoria $categoria)
+    {
+        $request->validate([
+            'nome'=>'required'
+        ],[
+            'nome.required'         => 'Por favor, escreva o nome da categoria'
+        ]);
+
+        try{
+
+            $categoria->fill($request->post())->update();
+
+            return response()->json([
+                'message'=>'Categoria atualizada com sucesso!'
+            ]);
+
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Ops! Algo de errado aconteceu ao tentar salvar o dado no banco. Por favor, tente novamente mais tarde!'
+            ],500);
+        }
+    }
+    public function destroy(Categoria $categoria)
+    {
+        try {
+            $categoria->delete();
+
+            return response()->json([
+                'message'=>'Categoria excluída com sucesso!'
+            ]);
+            
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Ops! Algo de errado aconteceu ao tentar excluir o dado no banco. Por favor, tente novamente mais tarde!'
+            ]);
+        }
+    }
+}
